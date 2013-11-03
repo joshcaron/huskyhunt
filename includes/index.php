219440dd4d7b18c0b99daad6abdc309bf2eaad9c
Josh Caron <jcaron@ccs.neu.edu>
2013-11-03 10:00:29 -0500

    Made includes folder the root

---
+++ includes/index.php
@@ -0,0 +1,4 @@
+<form enctype="multipart/form-data" action="/upload.php" method="post">
+	<input type="file" name="userfile" accept="image/*" capture="camera">
+	<input type="submit" value="Submit">
+</form>
