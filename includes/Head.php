c09ee7faf431cf6830ab4ed6fc1da6ec75819b9b
Josh Caron <jcaron@ccs.neu.edu>
2013-11-03 12:31:02 -0500

    Because why wouldn't you write your HTML in PHP

---
+++ includes/Head.php
@@ -0,0 +1,27 @@
+<?php
+$spreadsheet = '0Ajgj_dge0NVpdEpIZWk1MWVYZVNSMjh2SjY4VHhJVGc';
+
+?>
+<!DOCTYPE html>
+<head>
+	<title>The Upton Sinclairs</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/semantic.css">
+	<script type="text/javascript" src="/assets/js/spreadsheet.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="https://spreadsheets.google.com/feeds/list/<?php echo $spreadsheet; ?>/od6/public/basic?alt=json-in-script&callback=parseJSON"></script>
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Open+Sans:300italic,400,300,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+	<style type="text/css">
+	@font-face {
+		font-family: 'Icons';
+		src: url('/assets/fonts/icons.ttf');
+	}
+	</style>
+</head>
+<body>
+<div class="ui one column page grid">
+	<div class="column">
+		<h2 class="ui header">
+			<div class="content">Husky Hunt 2013</div>
+			<div class="sub header">Awesome tagline here</div>
+		</h2>
---
+++ includes/upload.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('display_errors', 1);
+// Current directory
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+// Extensions
+array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["userfile"]["name"]);
+$extension = end($temp);
+
+// Allowed file extensions
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+
+if (in_array($extension, $allowedExts)) {
+	if ($_FILES["userfile"]["error"] > 0) {
+		echo "Return Code: " . $_FILES["userfile"]["error"] . "<br>";
+	} else {
+
+		echo "Upload: " . $_FILES["userfile"]["name"] . "<br>";
+		echo "Type: " . $_FILES["userfile"]["type"] . "<br>";
+		echo "Size: " . ($_FILES["userfile"]["size"] / 1024) . " kB<br>";
+		echo "Temp file: " . $_FILES["userfile"]["tmp_name"] . "<br>";
+
+		if (file_exists("upload/" . $_FILES["userfile"]["name"])) {
+			echo $_FILES["userfile"]["name"] . " already exists. ";
+		} else {
+			move_uploaded_file($_FILES["userfile"]["tmp_name"], $root . "/images/" . $_FILES["userfile"]["name"]);
+			echo "Stored in: " . $root . "/images/" . $_FILES["userfile"]["name"];
+		}
+	}
+
+} else {
+	echo "Sorry Mike. :)";
+}
