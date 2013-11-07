d97874cf872a721e382a8c798f8cd7f16310435a
Josh Caron <jcaron@ccs.neu.edu>
2013-11-07 18:44:02 -0500

    Escaping spaces by urlencoding LOL

--- htdocs/upload.php
+++ htdocs/upload.php
@@ -16,20 +16,21 @@ if (in_array($extension, $allowedExts)) {
 		echo "Return Code: " . $_FILES["userfile"]["error"] . "<br>";
 	} else {
 
-		echo "Upload: " . $_FILES["userfile"]["name"] . "<br>";
+		$fileName = urlencode($_FILES["userfile"]["name"]);
+		echo "Upload: " . $fileName . "<br>";
 		echo "Type: " . $_FILES["userfile"]["type"] . "<br>";
 		echo "Size: " . ($_FILES["userfile"]["size"] / 1024) . " kB<br>";
 		echo "Temp file: " . $_FILES["userfile"]["tmp_name"] . "<br>";
 
-		if (file_exists("upload/" . $_FILES["userfile"]["name"])) {
-			echo $_FILES["userfile"]["name"] . " already exists. ";
+		if (file_exists("upload/" . $fileName)) {
+			echo $fileName . " already exists. ";
 		} else {
 			$uploadFolder = $root . "/uploads";
 			if (!file_exists($uploadFolder)) {
 			    mkdir('uploadFolder', 0777, true);
 			}
-			move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadFolder . "/" . $_FILES["userfile"]["name"]);
-			echo "Stored in: " . $root . "/uploads/" . $_FILES["userfile"]["name"];
+			move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadFolder . "/" . $fileName);
+			echo "Stored in: " . $root . "/uploads/" . $fileName;
 		}
 	}
 
