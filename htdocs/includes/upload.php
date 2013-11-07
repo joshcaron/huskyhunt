bd081b0a79e438fd13e799b4c252dbaf06ee4f7b
Josh Caron <jcaron@ccs.neu.edu>
2013-11-07 18:33:14 -0500

    Disable actual storing of file temporarily

--- htdocs/includes/upload.php
+++ htdocs/includes/upload.php
@@ -24,7 +24,7 @@ if (in_array($extension, $allowedExts)) {
 		if (file_exists("upload/" . $_FILES["userfile"]["name"])) {
 			echo $_FILES["userfile"]["name"] . " already exists. ";
 		} else {
-			move_uploaded_file($_FILES["userfile"]["tmp_name"], $root . "/images/" . $_FILES["userfile"]["name"]);
+			// move_uploaded_file($_FILES["userfile"]["tmp_name"], $root . "/images/" . $_FILES["userfile"]["name"]);
 			echo "Stored in: " . $root . "/images/" . $_FILES["userfile"]["name"];
 		}
 	}
