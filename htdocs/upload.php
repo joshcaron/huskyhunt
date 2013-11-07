b7b81efc49556a438be60a5030046c521dc0b8a3
Josh Caron <jcaron@ccs.neu.edu>
2013-11-07 18:38:31 -0500

    Fixed upload to store files

--- htdocs/upload.php
+++ htdocs/upload.php
@@ -24,8 +24,12 @@ if (in_array($extension, $allowedExts)) {
 		if (file_exists("upload/" . $_FILES["userfile"]["name"])) {
 			echo $_FILES["userfile"]["name"] . " already exists. ";
 		} else {
-			// move_uploaded_file($_FILES["userfile"]["tmp_name"], $root . "/images/" . $_FILES["userfile"]["name"]);
-			echo "Stored in: " . $root . "/images/" . $_FILES["userfile"]["name"];
+			$uploadFolder = $root . "/uploads";
+			if (!file_exists($uploadFolder)) {
+			    mkdir('uploadFolder', 0777, true);
+			}
+			move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadFolder . "/" . $_FILES["userfile"]["name"]);
+			echo "Stored in: " . $root . "/uploads/" . $_FILES["userfile"]["name"];
 		}
 	}
 
